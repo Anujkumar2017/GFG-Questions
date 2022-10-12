18d7dc3517fbd7287a614e22c22c6ab2104c23db
Anuj Kumar Maurya <78439576+Anujkumar2017@users.noreply.github.com>
2022-10-12 12:51:05 +0530

    Added solution - LeetHub

--- Save Your Life - GFG/save-your-life.cpp
+++ Save Your Life - GFG/save-your-life.cpp
@@ -9,6 +9,8 @@ using namespace std;
 
 class Solution{
 public:
+
+    // use kadene's algorithm for problem
       string maxSum(string w,char x[], int b[],int n){
           // code here
           int sum=0,mx=INT_MIN,l=0,r=0,mxl,mxr;
@@ -17,16 +19,14 @@ public:
           for(int i=0;i<n;i++){
                 mp[x[i]]=b[i];                    
           }
-          
+          //check all redefined asccii value for every element in string is negative   
           bool allnegative=true;
           
           for(int i=0;i<w.size();i++){
               if(mp[w[i]]>=0){
                   allnegative=false;
               }
-              //cout<<"ll "<<allnegative<<"\n";  
           }
-          //cout<<"ll "<<allnegative<<" ";
           
           if(allnegative==true){
             int ans=0;
@@ -40,27 +40,25 @@ public:
             //cout<<ch<<" ";
             return s;
           }else{
+              
+            //apply kadene algorithm here
             for(int i=0;i<w.size();i++){
               if(sum<0){
                   sum=0;
                   l=i;
               }
               sum+=mp[w[i]];
+              // if element's ASCII in not redefined
               if(mp[w[i]]==0)
                 sum+=(int)w[i];
               
               if(sum>mx){
                   mx=sum;
                   r=i;
+                  //keep the l and r for maximum sum 
                   mxl=l,mxr=r;
               }
-            //   cout<<sum<<" ";
-            //   cout<<l<<" "<<r<<"\n";
           }
-        //   cout<<"\n";
-        //cout<<mxl<<" "<<mxr<<" ";
-          if(l>r)
-            l=0;
           return w.substr(mxl,mxr-mxl+1);    
         }
           
