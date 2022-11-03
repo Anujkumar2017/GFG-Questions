4ca705a345942cf9593fdb968229e7087ee36370
Anuj Kumar Maurya <78439576+Anujkumar2017@users.noreply.github.com>
2022-11-03 20:28:57 +0530

    Time: 1047 ms (25.41%), Space: 168.1 MB (27.99%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -1,57 +1,66 @@
 class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
+        //map to count frequency of strings
         map<string,int> mp;
         for(string x:words){
             mp[x]++;
         }
         
+        /* we store strings which is palindrome itself i.e reverse is same to particular                string */ 
         vector<string> v;
         int ans=0,twice=0;
+        
         
         for(auto itr=mp.begin(); itr!=mp.end(); itr++){
             string st = itr->first,rev;
             rev=string(st.rbegin(),st.rend());
             
-            //cout<<mp[rev]<<" ";
+            // if string's reverse is same as string  
             if(st==rev){
                 v.push_back(st);
-                //itr->second=1;
             }else if(mp.find(rev)!=mp.end() && itr->second!=-1){
+                // if reverse is present in map and it is not same as string
+                
+                /* it's contibution in length is min of revers string's frequency and                          frequeny of string multiply by 4 */
                 ans+=min(mp[rev],itr->second)*4;
+                
+                // for preventing to check for reverse of string
                 mp[rev]=-1;
             }
-            //cout<<ans<<"\n";
         }
+        // maximum odd frequency in  palindrome strings
         int mxodd=-1;
         
+        //calculation of mxodd
         for(auto x:mp){
-            cout<<x.first<<" ";
+            //cout<<x.first<<" ";
             string st=x.first;
             string rev=string(st.rbegin(),st.rend());
             
             if(x.second%2!=0 && x.first==rev){
                 mxodd=max(mxodd,x.second);
             }
-            cout<<x.second<<"\n";
+            //cout<<x.second<<"\n";
         }
         
+        // traversing in vector of palindrome strings
         for(auto x:v){
+            // if fequency is even
             if(mp[x]%2==0){
                 ans+=mp[x]*2;
             }
             else{
+                //calculation of mxodd only 1 time
                 if(mp[x]==mxodd && twice!=1){
                     ans+=mxodd*2;
                     twice=1;
                 }else{
+                    // if mxodd in present many time or frequency is odd 
                     ans+=(mp[x]-1)*2;
                 }
             }
         }
-        
-        // if(v.size()==1)
-        //     ans+=2;
         
         return ans;
     }
