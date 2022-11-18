0991549e8809d7f6a4396e4d49e1c5f95c65d687
Anuj Kumar Maurya <78439576+Anujkumar2017@users.noreply.github.com>
2022-11-18 08:17:15 +0530

    Added solution - LeetHub

---
+++ Number Of Open Doors - GFG/number-of-open-doors.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int noOfOpenDoors(long long N) {
+        // code here
+
+        // Number which has odd factor will open at end of procedure 
+        // Factor of pefect square number is odd 
+        
+        // ans is finding the nearest perfect square of the number i.e sqrt(n);
+        return sqrt(N);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.noOfOpenDoors(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
