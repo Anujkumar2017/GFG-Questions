3dac9b39271d0b1590ca49f39150793251ec10b4
Anuj Kumar Maurya <78439576+Anujkumar2017@users.noreply.github.com>
2023-09-16 22:39:49 +0530

    Added solution - LeetHub

---
+++ Game with String - GFG/game-with-string.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int minValue(string s, int k){
+        // code here
+        map<char,int> mp;
+        priority_queue<int> pq;
+        
+        for(auto ch:s){
+            mp[ch]++;
+        }
+        
+        for(auto it:mp){
+            pq.push(it.second);
+        }
+        
+        while(k--){
+            int temp=pq.top();
+            pq.pop();
+            
+            if(temp>1)
+                pq.push(temp-1);
+        }
+        
+        int ans=0;
+        while(!pq.empty()){
+            int x=pq.top();
+            ans+=x*x;
+            pq.pop();
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        int k;
+        cin>>s>>k;
+        
+        Solution ob;
+        cout<<ob.minValue(s, k)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
