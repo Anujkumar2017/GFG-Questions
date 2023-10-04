e866068c9fe94a7e6926c905d3f06bd180f83d83
Anuj Kumar Maurya <78439576+Anujkumar2017@users.noreply.github.com>
2023-10-04 18:14:06 +0530

    Added solution - LeetHub

---
+++ First and last Bit - GFG/first-and-last-bit.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	int onlyFirstAndLastAreSet(long long int n) { 
+	    //complete the function here
+	    if(n%2==0)
+	        return 0;
+	    return ((n-1)& (n-2))==0;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        long long n;
+        cin >> n;
+        Solution ob;
+        cout << ob.onlyFirstAndLastAreSet(n) << endl;
+    }
+    
+return 0;
+}
+
+
+// } Driver Code Ends
