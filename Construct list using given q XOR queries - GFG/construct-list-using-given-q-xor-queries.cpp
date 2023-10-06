2a63047767d827431eec789c40277db37607073d
Anuj Kumar Maurya <78439576+Anujkumar2017@users.noreply.github.com>
2023-10-06 23:24:00 +0530

    Added solution - LeetHub

---
+++ Construct list using given q XOR queries - GFG/construct-list-using-given-q-xor-queries.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    vector<int> constructList(vector<vector<int>> Q, int N) {
+        // code here
+        int x=0;
+        vector<int> ans;
+        
+        for(int i=N-1; i>=0; i--){
+            if(Q[i][0]==0){
+                ans.push_back(x^Q[i][1]);
+            }else 
+                x=x^Q[i][1];
+        }
+        
+        ans.push_back(x);
+        sort(ans.begin(), ans.end());
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,type,val;
+        cin>>N;
+        
+        vector<vector<int>> Q;
+        
+        for(int i=0; i<N; i++)
+        {
+            cin>>type>>val;
+            vector<int> temp;
+            temp.push_back(type);
+            temp.push_back(val);
+            Q.push_back(temp);
+        }
+
+        Solution ob;
+        vector<int> res = ob.constructList(Q,N);
+        
+        for(int i=0; i<res.size(); i++)
+            cout<<res[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
